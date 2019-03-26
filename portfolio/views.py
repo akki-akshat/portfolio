31f7b33e158b066bd305e9844834786183f8ff9f
Akshat Ahuja <akshatahuja03@gmail.com>
2019-03-27 00:03:00 +0530

    adding hello world

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your view0
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
