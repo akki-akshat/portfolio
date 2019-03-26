ecad58853b5b2fcf3775f32e34a6b73e3654e13c
Akshat Ahuja <akshatahuja03@gmail.com>
2019-03-27 00:07:22 +0530

    changes in setting file

--- MainApp/settings.py
+++ MainApp/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-# import mongoengine
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -99,7 +98,7 @@ DATABASES = {
         # 'NAME': 'portfolio',
     }
 }
-SESSION_ENGINE = 'mongoengine.django.sessions'
+# SESSION_ENGINE = 'mongoengine.django.sessions'
 
 
 # Password validation
@@ -139,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/static/"
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 STATICFILES_DIRS = (
-    "/static/",
+    os.path.join(BASE_DIR, "/static/"),
 )
