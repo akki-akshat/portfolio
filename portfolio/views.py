e8a4b01f72d6885b266f802e18e6844e44c2b4a4
Ashwin Khushlani <ashwink.ak62@gmail.com>
2019-04-05 18:23:56 +0530

    Commit from Ashwin's Laptop

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your view0
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the index of my potfolio.")
