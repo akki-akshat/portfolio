6209012102bb830aaecfdaaf8f58530be9c1d221
Akshat Ahuja <akshatahuja03@gmail.com>
2019-04-07 03:57:49 +0530

    added demo UI with respective static files and images

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your view0
+# def index(request):
+    # return HttpResponse("Hello, world. You're at the index of my potfolio.")
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the index of my potfolio.")
+    template = "index.html"
+    return  render(request, template, context=None)
