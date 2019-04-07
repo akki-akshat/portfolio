bd3240fe9e037e7a4f263e76caa1c720fcbb0fa6
Akshat Ahuja <akshatahuja03@gmail.com>
2019-04-07 06:15:45 +0530

    added changes to index file with personalised data

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 # Create your view0
 # def index(request):
     # return HttpResponse("Hello, world. You're at the index of my potfolio.")
 
 def index(request):
+    with open('portfolio/akshat_context.json') as json_file:  
+        data = json.load(json_file) 
     template = "index.html"
-    return  render(request, template, context=None)
+    return  render(request, template, context={"data":data, "range":range(5),})
