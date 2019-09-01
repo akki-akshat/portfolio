f13eb11b367fb9f2d3ae8a53498cdd97789bd859
Akshat Ahuja( I516378) <akshat.ahuja@sap.com>
2019-09-01 17:07:19 +0530

    Added content and api

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 import json
 
 # Create your view0
@@ -7,7 +8,7 @@ import json
     # return HttpResponse("Hello, world. You're at the index of my potfolio.")
 
 def index(request):
-    with open('portfolio/akshat_context.json') as json_file:  
-        data = json.load(json_file) 
+    # with open('https://api.mlab.com/api/1/databases/sap_intro/collections/akki?apiKey=uRF8yptjRhXgGAK3wUNUrOawnwcK4tzV') as json_file:  
+    data = requests.get('http://akshatahuja-eval-test.apigee.net/myintro').json()
     template = "index.html"
-    return  render(request, template, context={"data":data, "range":range(5),})
+    return  render(request, template, context={"data":data[0]})
